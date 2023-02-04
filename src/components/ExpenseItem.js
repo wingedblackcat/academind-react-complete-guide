23fabeabb04edd012ee0ff642e7147acf5c5b5e7
Michael Sayapin <michaelsay95@gmail.com>
2023-02-04 16:55:04 +0200

    feat: configure date UI

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,10 +6,17 @@ import './ExpenseItem.css';
  */
 function ExpenseItem(props) {
   const { title, date, amount } = props;
+  const year = date.getFullYear();
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
 
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
