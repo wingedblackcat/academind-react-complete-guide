83f57928f85d64b8b6df1a5c3176ff6c849693af
Michael Sayapin <michaelsay95@gmail.com>
2023-02-04 15:51:47 +0200

    feat: create and use the Expense Item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h1>Let's get started</h1>
+        <ExpenseItem></ExpenseItem>
     </div>
   );
 }
