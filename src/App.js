dbf0b3988df4922306d50932981674e8008815ef
Michael Sayapin <michaelsay95@gmail.com>
2023-02-04 21:15:08 +0200

    feat: create an expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 /**
  * @type {Expense[]}
@@ -28,7 +29,7 @@ const expenses = [
 const App = () => {
   return (
     <div>
-      <h1>Let's get started</h1>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
