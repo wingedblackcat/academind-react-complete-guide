fb8c1d97d855f96ee30cc33de5c73fccb23cd7f7
Michael Sayapin <michaelsay95@gmail.com>
2023-02-06 20:50:06 +0200

    feat: create the chart component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,31 @@
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+/**
+ * @param {ChartProps} props
+ * @returns {JSX.Element}
+ */
+const Chart = (props) => {
+  const { dataPoints } = props;
+
+  return (
+    <div className="chart">
+      {dataPoints.map((dataPoint) => (
+        <ChartBar
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+          key={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
+
+/**
+ * @typedef ChartProps
+ * @type object
+ * @property {DataPoint[]} dataPoints
+ */
